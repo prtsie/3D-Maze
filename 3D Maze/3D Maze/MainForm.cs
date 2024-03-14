a6ec063c69f2b6fad36a85ce3e65ec94e6c23589
prtsie <yonhsup@mail.ru>
2024-03-15 00:29:33 +0300

    2d

---
+++ 3D Maze/3D Maze/MainForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3D_Maze
+{
+    public partial class MainForm : Form
+    {
+        private readonly Graphics graphics;
+        private readonly Pen bordersPen = Pens.Black;
+        private readonly Brush fillBrush = Brushes.Orange;
+        private readonly Maze maze;
+        private const int BlockSize = 25;
+        private readonly int size = 9;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            graphics = CreateGraphics();
+            maze = new Maze(ref size);
+        }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            for (var row = 0; row < size; row++)
+            {
+                for (var col = 0; col < size; col++)
+                {
+                    if (maze.Walls[row, col])
+                    {
+                        graphics.DrawRectangle(bordersPen, new Rectangle(BlockSize * col, BlockSize * row, BlockSize, BlockSize));
+                        graphics.FillRectangle(fillBrush, new Rectangle(BlockSize * col, BlockSize * row, BlockSize, BlockSize));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 3D Maze/3D Maze/Maze.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3D_Maze
+{
+    internal sealed class Maze
+    {
+        private const int GeneratorStep = 2;
+        private const int WallsThickness = GeneratorStep - 1;
+        private readonly static Random randomizer = new();
+        private readonly Point start = new(WallsThickness, WallsThickness);
+
+        public bool[,] Walls { get; }
+
+        public Maze(ref int size)
+        {
+            if (size % 2 == 0)
+            {
+                size++;
+            }
+
+            Walls = new bool[size + WallsThickness * 2, size + WallsThickness * 2];
+            size += WallsThickness * 2;
+            InitWalls();
+            Generate(start);
+        }
+
+        private void Generate(Point from)
+        {
+            //Make start
+            Walls[start.Y, start.X] = false;
+
+            var toVisitList = GetNeighbors(from);
+            while (toVisitList.Count > 0)
+            {
+                var cellToVisit = toVisitList[randomizer.Next(toVisitList.Count)];
+                if (Walls[cellToVisit.Y, cellToVisit.X])
+                {
+                    Visit(cellToVisit, from);
+                    Generate(cellToVisit);
+                }
+
+                toVisitList.Remove(cellToVisit);
+            }
+        }
+
+        private void InitWalls()
+        {
+            for (var row = 0; row < Walls.GetLength(0); row++)
+            {
+                for (var col = 0; col < Walls.GetLength(1); col++)
+                {
+                    Walls[row, col] = true;
+                }
+            }
+        }
+
+        private List<Point> GetNeighbors(Point point)
+        {
+            var length = Walls.GetLength(0);
+            var list = new List<Point>();
+            if (point.X - GeneratorStep >= 0)
+            {
+                list.Add(point with { X = point.X - GeneratorStep });
+            }
+
+            if (point.X + GeneratorStep < length)
+            {
+                list.Add(point with { X = point.X + GeneratorStep });
+            }
+
+            if (point.Y - GeneratorStep >= 0)
+            {
+                list.Add(point with { Y = point.Y - GeneratorStep });
+            }
+
+            if (point.Y + GeneratorStep < length)
+            {
+                list.Add(point with { Y = point.Y + GeneratorStep });
+            }
+
+            return list;
+        }
+
+        private void Visit(Point point, Point from)
+        {
+            var rowMultiplier = from.Y < point.Y ? 1 : -1;
+            var colMultiplier = from.X < point.X ? 1 : -1;
+            for (var row = from.Y; row != point.Y; row += 1 * rowMultiplier)
+            {
+                Walls[row, from.X] = false;
+            }
+
+            for (var col = from.X; col != point.X; col += 1 * colMultiplier)
+            {
+                Walls[from.Y, col] = false;
+            }
+
+            Walls[point.Y, point.X] = false;
+        }
+    }
+}
